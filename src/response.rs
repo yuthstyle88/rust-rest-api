80741c542409a3ec2a0ee04078583d53c892075e
the style88 <the_style88@icounds.com>
2021-07-30 15:18:24 +0700

    fix post json

--- src/response.rs
+++ src/response.rs
@@ -1,5 +1,5 @@
 use serde::{Deserialize, Serialize};
-
+use actix_web::http::StatusCode;
 #[macro_export]
 macro_rules! res {
     ($x:expr) => {web::Json(JsonOk::ok($x))}
@@ -38,125 +38,9 @@ impl<K, T> Json<K, T> {
 impl<'a, T> JsonOk<'a, T> {
     pub fn ok(data: T) -> JsonOk<'a, T> {
         JsonOk {
-            code: Code::Ok as u16,
+            code: StatusCode::OK.as_u16(),
             message: "ok",
             data,
         }
     }
-}
-
-
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub enum Code {
-    // 2xx success
-    Ok = 200,
-    Created = 201,
-    Accepted = 202,
-    NonAuthoritativeInformation = 203,
-    NoContent = 204,
-    ResetContent = 205,
-    PartialContent = 206,
-    MultiStatus = 207,
-    AlreadyReported = 208,
-    IMUsed = 226,
-
-    // 3xx redirection
-    MultipleChoices = 300,
-    MovedPermanently = 301,
-    MovedTemporarily = 302,
-    SeeOther = 303,
-    NotModified = 304,
-    UseProxy = 305,
-    SwitchProxy = 306,
-    TemporaryRedirect = 307,
-    PermanentRedirect = 308,
-
-    // 4xx client errors
-    BadRequest = 400,
-    Unauthorized = 401,
-    PaymentRequired = 402,
-    Forbidden = 403,
-    NotFound = 404,
-    MethodNotAllowed = 405,
-    NotAcceptable = 406,
-    // 407 Proxy Authentication Required
-    RequestTimeout = 408,
-    Conflict = 409,
-    // 410 Gone
-    // 411 Length Required
-    // 412 Precondition Failed
-    // 413 Payload Too Large
-    // 414 URI Too Long
-    UnsupportedMediaType = 415,
-    // 416 Range Not Satisfiable
-    // 417 Expectation Failed\
-    // 418 I'm a teapot (RFC 2324, RFC 7168)
-    // 421 Misdirected Request
-    // 422 Unprocessable Entity
-    // 423 Locked
-    // 424 Failed Dependency
-    // 425 Too Early
-    // 426 Upgrade Required
-    // 428 Precondition Required
-    // 429 Too Many Requests
-    // 431 Request Header Fields Too Large
-    UnavailableForLegalReasons = 451,
-
-    // 5xx server errors
-    InternalServerError = 500,
-    NotImplemented = 501,
-    // 502 Bad Gateway
-    ServiceUnavailable = 503,
-    // 505 HTTP Version Not Supported
-    // 506 Variant Also Negotiates
-    // 507 Insufficient Storage
-    // 508 Loop Detected
-    // 510 Not Extended
-    // 511 Network Authentication Required
-
-    // Unofficial codes
-    Checkpoint = 103,
-    ThisIsFine = 218,
-    // 419 Page Expired
-    // 420 Method Failure
-    // 420 Enhance Your Calm
-    // 430 Request Header Fields Too Large
-    // 450 Blocked by Windows Parental Controls (Microsoft)
-    // 498 Invalid Token (Esri)
-    // 499 Token Required (Esri)
-    // 509 Bandwidth Limit Exceeded
-    // 526 Invalid SSL Certificate
-    // 529 Site is overloaded
-    // 530 Site is frozen
-    // 598 (Informal convention) Network read timeout error
-
-    // Internet Information Services
-    // 440 Login Time-out
-    // 449 Retry With
-    // 451 Redirect
-
-    // nginx
-    // 444 No Response
-    // 494 Request header too large
-    // 495 SSL Certificate Error
-    // 496 SSL Certificate Required
-    // 497 HTTP Request Sent to HTTPS Port
-    // 499 Client Closed Request
-
-    // Cloudflare
-    // 520 Web Server Returned an Unknown Error
-    // 521 Web Server Is Down
-    // 522 Connection Timed Out
-    // 523 Origin Is Unreachable
-    // 524 A Timeout Occurred
-    // 525 SSL Handshake Failed
-    // 526 Invalid SSL Certificate
-    // 527 Railgun Error
-    // 530 Error 530 is returned along with a 1xxx error
-
-    // AWS Elastic Load Balancer
-    // 460 Client closed the connection with the load balancer before the idle timeout period elapsed. Typically when client timeout is sooner than the Elastic Load Balancer's timeout.
-    // 463 The load balancer received an X-Forwarded-For request header with more than 30 IP addresses.
-    // 561 Unauthorized
 }
